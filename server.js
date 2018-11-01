d3790d9ac1f1de87f249cc519ca5c8c07c3c499c
mhshifat <mhgamingbd02@gmail.com>
2018-11-01 23:48:49 +0600

    Fixed express static folder for production

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get("/api", (req, res) => {
 
 // For Production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
+  app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
