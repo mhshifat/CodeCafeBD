93cd94bba01b1e6b969fe33c509a83e7f9155d5e
mhshifat <mhgamingbd02@gmail.com>
2018-11-11 21:05:16 +0600

    Fixed project animation

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -3,11 +3,4 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./Reducers/rootReducer";
 
-export default createStore(
-  rootReducer,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+export default createStore(rootReducer, {}, compose(applyMiddleware(thunk)));
